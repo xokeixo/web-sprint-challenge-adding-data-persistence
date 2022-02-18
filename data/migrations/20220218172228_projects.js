65c85bed4dc20e9843bc78f8dbc558e900ab68b8
Markeisha <markeisha94@gmail.com>
2022-02-18 16:14:29 -0600

    undid changes

--- data/migrations/20220218172228_projects.js
+++ data/migrations/20220218172228_projects.js
@@ -3,54 +3,44 @@
  * @returns { Promise<void> }
  */
 exports.up = async function(knex) {
-    await knex.schema
-    .createTable('projects', (table) => {
-        table.increments('project_id')
-        table.string('project_name', 128)
-            .notNullable()
-        table.string('project_description', 255)
-        table.boolean('project_completed')
-            .defaultTo(false)
+    return knex.schema
+    .createTable('projects', tbl => {
+        tbl.increments('project_id');
+        tbl.string('project_name').notNullable();
+        tbl.string('project_description');
+        tbl.boolean('project_completed').defaultTo(0);
     })
-    .createTable('resources', (table) => {
-        table.increments('resource_id')
-        table.string('resource_name', 128)
-            .notNullable()
-            .unique()
-        table.string('resource_description', 255)
+    .createTable('resources', tbl => {
+        tbl.increments('resource_id');
+        tbl.string('resource_name').notNullable().unique();
+        tbl.string('resource_description');
     })
-    .createTable('tasks', (table) => {
-        table.increments('task_id')
-        table.string('task_description', 255)
-            .notNullable()
-        table.string('task_notes', 255)
-        table.boolean('task_completed')
-            .defaultTo(false)
-        table.integer('project_id')
+    .createTable('tasks', tbl=> {
+        tbl.increments('task_id');
+        tbl.string('task_description').notNullable();
+        tbl.string('task_notes');
+        tbl.boolean('task_completed').defaultTo(0);
+        tbl.integer('project_id')
             .unsigned()
             .notNullable()
             .references('project_id')
             .inTable('projects')
-            .onDelete('RESTRICT')
-            .onUpdate('RESTRICT')
+            .onDelete('CASCADE')
+            .onUpdate('CASCADE');
     })
-    .createTable('project_resources', (table) => {
-        table.increments('project_resource_id')
-        table.integer('project_id')
-            .notNullable()
+    .createTable('resource_assignment', tbl=> {
+        tbl.integer('resource_id')
             .unsigned()
-            .references('project_id')
-            .inTable('projects')
-            .onDelete('RESTRICT')
-            .onUpdate('RESTRICT')
-        table.integer('resource_id')
             .notNullable()
-            .unsigned()
             .references('resource_id')
-            .inTable('resources')
-            .onDelete('RESTRICT')
-            .onUpdate('RESTRICT')
-    })
+            .inTable('resources');
+        tbl.integer('project_id')
+            .unsigned()
+            .notNullable()
+            .references('project_id')
+            .inTable('projects');
+        tbl.primary(['resource_id', 'project_id']);
+    });
 };
 
 /**
